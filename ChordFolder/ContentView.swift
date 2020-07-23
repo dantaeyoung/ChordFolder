8286e0e1614a75001f45ea6ad5dafc6613f4252e
Dan Taeyoung <provolot@gmail.com>
2020-07-23 00:03:44 -0400

    Initial Commit

---
+++ ChordFolder/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ChordFolder
+//
+//  Created by Dan Taeyoung on 7/23/20.
+//  Copyright © 2020 Dan Taeyoung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
