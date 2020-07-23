b320c6672647c0e1e173ae0042d2f1e6a97c7fa0
Dan Taeyoung <provolot@gmail.com>
2020-07-23 01:49:47 -0400

    initial commit

--- ChordFolder/AppDelegate.swift
+++ ChordFolder/AppDelegate.swift
@@ -1,39 +1,95 @@
-//
-//  AppDelegate.swift
-//  ChordFolder
-//
-//  Created by Dan Taeyoung on 7/23/20.
-//  Copyright © 2020 Dan Taeyoung. All rights reserved.
-//
-
 import Cocoa
 import SwiftUI
+import HotKey
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
-
-
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
 
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 500)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Icon")
+            button.action = #selector(togglePopover(_:))
+        }
+        
+    
+        hotKey1 = HotKey(key: .f1, modifiers: [])
+        hotKey1!.keyDownHandler = {
+            self.openFolder(keyn: 1)
+        }
+        hotKey2 = HotKey(key: .f2, modifiers: [])
+        hotKey2!.keyDownHandler = {
+            self.openFolder(keyn: 2)
+        }
+        hotKey3 = HotKey(key: .f3, modifiers: [])
+        hotKey3!.keyDownHandler = {
+            self.openFolder(keyn: 3)
+        }
+        
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+                self.popover.contentViewController?.view.window?.becomeKey()
+            }
+        }
     }
-
-
+    
+    func openFolder(keyn: integer_t) {
+        print(keyn.description + "Pressed at \(Date())")
+        print(NSWorkspace.shared.frontmostApplication)
+        /*
+        guard let url = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.apple.Finder") else { return }
+        let path = "/bin"
+        let configuration = NSWorkspace.OpenConfiguration()
+        configuration.arguments = [path]
+        NSWorkspace.shared.openApplication(at: url,
+                                           configuration: configuration,
+                                           completionHandler: nil)
+    */
+     //   NSWorkspace.shared.openFile("/Users/provolot/Documents/")
+        //NSWorkspace.shared.selectFile(nil, inFileViewerRootedAtPath: "/Users/")
+        
+        let dirpath = "/Users/provolot/Downloads/"
+        let url = URL(fileURLWithPath: dirpath)
+        self.openFinder(url: url)
+        
+    }
+    
+    func openFinder(url: URL?){
+        guard let url = url else { return }
+        NSWorkspace.shared.activateFileViewerSelecting([url])
+    }
+    
+    public var hotKey1: HotKey?
+    public var hotKey2: HotKey?
+    public var hotKey3: HotKey?
+    
+    
 }
 
+struct AppDelegate_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
--- ChordFolder/ContentView.swift
+++ ChordFolder/ContentView.swift
@@ -8,10 +8,26 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Text("Hello, World!!!")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Button(action: {
+                buttonPress()
+            }) {
+                Text("button me")
+            }
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            Button("choose dir") {
+                let chosend = chooseDir()
+                print(chosend)
+                
+            }
+        }
     }
 }
 
@@ -20,4 +36,45 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+func chooseDir() -> String {
+    let dialog = NSOpenPanel();
+
+    dialog.title                   = "Choose a dir";
+    dialog.showsResizeIndicator    = true;
+    dialog.allowsMultipleSelection = false;
+    dialog.canChooseFiles = false;
+    dialog.canChooseDirectories = true;
+
+    if (dialog.runModal() ==  NSApplication.ModalResponse.OK) {
+        let result = dialog.url // Pathname of the file
+
+        if (result != nil) {
+            let path: String = result!.path
+            return path
+        }
+        
+    } else {
+        // User clicked on "Cancel"
+        return ""
+    }
+    
+    return ""
+}
+
+func buttonPress() -> Bool {
+    let myAppleScript = "display alert \"yo\""
+    var error: NSDictionary?
+    if let scriptObject = NSAppleScript(source: myAppleScript) {
+        if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error) {
+            print(output.stringValue)
+            return true
+        } else if (error != nil) {
+            print("error: \(error)")
+            return false
+        }
+    }
+    return true
 }
